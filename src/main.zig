bb22a9c73611d0d12445c81807fc9f94fca9416b
01xz <73350430+01xz@users.noreply.github.com>
2023-12-09 12:05:33 +0800

    fix: write out in rows

--- src/main.zig
+++ src/main.zig
@@ -7,8 +7,8 @@ pub fn writePPM(writer: anytype) !void {
 
     try writer.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
 
-    for (0..image_width) |i| {
-        for (0..image_height) |j| {
+    for (0..image_height) |j| {
+        for (0..image_width) |i| {
             const r: f64 = @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(image_width - 1));
             const g: f64 = @as(f64, @floatFromInt(j)) / @as(f64, @floatFromInt(image_height - 1));
             const b: f64 = 0.25;
@@ -23,7 +23,10 @@ pub fn writePPM(writer: anytype) !void {
 pub fn main() !void {
     var buffer_writer = std.io.bufferedWriter(std.io.getStdOut().writer());
     var writer = buffer_writer.writer();
+
     try writePPM(&writer);
+
     try buffer_writer.flush();
+
     std.debug.print("writing PPM done\n", .{});
 }
