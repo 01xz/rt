31d8f9a5bc19fc6f611216c6259c7110ee850d26
01xz <73350430+01xz@users.noreply.github.com>
2023-12-12 18:26:51 +0800

    feat: new `Camera` type

---
+++ src/Camera.zig
@@ -0,0 +1,118 @@
+const Camera = @This();
+
+const std = @import("std");
+const hittable = @import("hittable.zig");
+const vec3 = @import("vec3.zig");
+
+const Ray = @import("Ray.zig");
+const Interval = @import("Interval.zig");
+const Allocator = std.mem.Allocator;
+const HitRecord = hittable.HitRecord;
+const HittableList = hittable.HittableList;
+const Vec3 = vec3.Vec3;
+const Color = Vec3(f64);
+const Point = Vec3(f64);
+
+aspect_ratio: f64,
+
+image_width: u32,
+image_height: u32,
+
+centor: Point,
+
+pixel00_loc: Point,
+
+pixel_delta_u: Vec3(f64),
+pixel_delta_v: Vec3(f64),
+
+pub fn init(aspect_ratio: f64, image_width: u32) Camera {
+    const image_height: u32 = blk: {
+        const height: u32 = @intFromFloat(@as(f64, @floatFromInt(image_width)) / aspect_ratio);
+        break :blk if (height < 1) 1 else height;
+    };
+
+    const focal_length: f64 = 1.0;
+
+    const viewport_height: f64 = 2.0;
+    const viewport_width: f64 = viewport_height *
+        (@as(f64, @floatFromInt(image_width)) / @as(f64, @floatFromInt(image_height)));
+
+    const centor = Point.at(0, 0, 0);
+
+    // viewport edges
+    const viewport_u = Point.at(viewport_width, 0.0, 0.0);
+    const viewport_v = Point.at(0.0, -viewport_height, 0.0);
+
+    // pixel delta
+    const pixel_delta_u = viewport_u.div(@floatFromInt(image_width));
+    const pixel_delta_v = viewport_v.div(@floatFromInt(image_height));
+
+    const viewport_upper_left = centor
+        .vsub(&Point.at(0, 0, focal_length))
+        .vsub(&viewport_u.div(2.0))
+        .vsub(&viewport_v.div(2.0));
+
+    const pixel00_loc = viewport_upper_left.vadd(&pixel_delta_u.vadd(&pixel_delta_v).mul(0.5));
+
+    return .{
+        .aspect_ratio = aspect_ratio,
+        .image_width = image_width,
+        .image_height = image_height,
+        .centor = centor,
+        .pixel00_loc = pixel00_loc,
+        .pixel_delta_u = pixel_delta_u,
+        .pixel_delta_v = pixel_delta_v,
+    };
+}
+
+pub fn render(self: *const Camera, world: *const HittableList, allocator: Allocator) ![][]i64 {
+    var colored_pixels = try allocator.alloc([]i64, self.image_height);
+    for (colored_pixels) |*item| {
+        item.* = try allocator.alloc(i64, self.image_width);
+    }
+
+    for (0..self.image_height) |j| {
+        for (0..self.image_width) |i| {
+            const pixel_centor = self.pixel00_loc
+                .vadd(&self.pixel_delta_u.mul(@floatFromInt(i)))
+                .vadd(&self.pixel_delta_v.mul(@floatFromInt(j)));
+            const ray_direction = pixel_centor.vsub(&self.centor);
+            const ray = Ray.init(self.centor, ray_direction);
+            const color = rayColor(&ray, world);
+            colored_pixels[j][i] = writeColor(&color);
+        }
+    }
+
+    return colored_pixels;
+}
+
+fn rayColor(ray: *const Ray, world: *const HittableList) Color {
+    const inf = std.math.inf(f64);
+
+    var rec: HitRecord = undefined;
+
+    // normals-colored world
+    if (world.hit(ray, Interval.init(0.0, inf), &rec)) {
+        return Color.at(1.0, 1.0, 1.0).vadd(&rec.normal).mul(0.5);
+    }
+
+    const color_start = Color.at(1.0, 1.0, 1.0);
+    const color_end = Color.at(0.5, 0.7, 1.0);
+
+    const ray_unit_vec = ray.direction.unit();
+
+    // scale `y()` to [0.0, 1.0]
+    const a = 0.5 * (ray_unit_vec.y() + 1.0);
+
+    // (1 - a) * start + a * end
+    return color_start.mul(1.0 - a).vadd(&color_end.mul(a));
+}
+
+inline fn writeColor(color: *const Color) i64 {
+    const icolor = Vec3(i64).at(
+        @intFromFloat(255.999 * color.x()),
+        @intFromFloat(255.999 * color.y()),
+        @intFromFloat(255.999 * color.z()),
+    );
+    return icolor.x() << 16 | icolor.y() << 8 | icolor.z();
+}
--- src/main.zig
+++ src/main.zig
@@ -1,96 +1,29 @@
 const std = @import("std");
 const config = @import("config");
+const vec3 = @import("vec3.zig");
 const hittable = @import("hittable.zig");
 
-const Vec3 = @import("vec3.zig").Vec3;
-const Point = Vec3(f64);
-const Color = Vec3(f64);
-const Ray = @import("Ray.zig");
-const Interval = @import("Interval.zig");
-const HittableList = hittable.HittableList;
+const Camera = @import("Camera.zig");
 const HitRecord = hittable.HitRecord;
-const Hittable = hittable.Hittable;
+const HittableList = hittable.HittableList;
 const Sphere = hittable.Sphere;
+const Vec3 = vec3.Vec3;
+const Color = Vec3(f64);
+const Point = Vec3(f64);
 
-// constants
-const pi: f64 = 3.1415926535897932385;
-const inf = std.math.inf(f64);
+fn writePPM(writer: anytype, colored_pixels: [][]const i64) !void {
+    const image_height = colored_pixels.len;
+    const image_width = colored_pixels[0].len;
 
-// image
-const aspect_ratio: f64 = 16.0 / 9.0;
-
-const image_width: u32 = 1920;
-const image_height: u32 = blk: {
-    const height: u32 = @intFromFloat(@as(f64, @floatFromInt(image_width)) / aspect_ratio);
-    break :blk if (height < 1) 1 else height;
-};
-
-// camera
-const focal_length: f64 = 1.0;
-
-const viewport_height: f64 = 2.0;
-const viewport_width: f64 = viewport_height *
-    (@as(f64, @floatFromInt(image_width)) / @as(f64, @floatFromInt(image_height)));
-
-const camera_centor: Point = Point.at(0.0, 0.0, 0.0);
-
-// viewport edges
-const viewport_u: Point = Point.at(viewport_width, 0.0, 0.0);
-const viewport_v: Point = Point.at(0.0, -viewport_height, 0.0);
-
-// pixel delta
-const pixel_delta_u: Point = viewport_u.div(@floatFromInt(image_width));
-const pixel_delta_v: Point = viewport_v.div(@floatFromInt(image_height));
-
-// locations
-const viewport_upper_left: Point = camera_centor
-    .vsub(&Point.at(0, 0, focal_length))
-    .vsub(&viewport_u.div(2.0))
-    .vsub(&viewport_v.div(2.0));
-
-const pixel00_loc: Point = viewport_upper_left.vadd(&pixel_delta_u.vadd(&pixel_delta_v).mul(0.5));
-
-pub fn rayColor(ray: *const Ray, world: *const HittableList) Color {
-    var rec: HitRecord = undefined;
-
-    // normals-colored world
-    if (world.hit(ray, Interval.init(0.0, inf), &rec)) {
-        return Color.at(1.0, 1.0, 1.0).vadd(&rec.normal).mul(0.5);
-    }
-
-    const color_start = Color.at(1.0, 1.0, 1.0);
-    const color_end = Color.at(0.5, 0.7, 1.0);
-
-    const ray_unit_vec = ray.direction.unit();
-
-    // scale `y()` to [0.0, 1.0]
-    const a = 0.5 * (ray_unit_vec.y() + 1.0);
-
-    // (1 - a) * start + a * end
-    return color_start.mul(1.0 - a).vadd(&color_end.mul(a));
-}
-
-pub fn writeColor(writer: anytype, color: *const Color) !void {
-    const icolor = Vec3(u8).at(
-        @intFromFloat(255.999 * color.x()),
-        @intFromFloat(255.999 * color.y()),
-        @intFromFloat(255.999 * color.z()),
-    );
-    try writer.print("{d} {d} {d}\n", .{ icolor.x(), icolor.y(), icolor.z() });
-}
-
-pub fn writePPM(writer: anytype, world: *const HittableList) !void {
     try writer.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
 
-    for (0..image_height) |j| {
-        for (0..image_width) |i| {
-            const pixel_centor = pixel00_loc
-                .vadd(&pixel_delta_u.mul(@floatFromInt(i)))
-                .vadd(&pixel_delta_v.mul(@floatFromInt(j)));
-            const ray_direction = pixel_centor.vsub(&camera_centor);
-            const ray = Ray.init(camera_centor, ray_direction);
-            const color = rayColor(&ray, world);
-            try writeColor(writer, &color);
+    for (colored_pixels) |row| {
+        for (row) |p| {
+            try writer.print("{d} {d} {d}\n", .{
+                (p >> 16) & 0xff,
+                (p >> 8) & 0xff,
+                p & 0xff,
+            });
         }
     }
 }
@@ -102,17 +35,21 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
 
-    var allocator = arena.allocator();
+    const allocator = arena.allocator();
 
-    // world
+    // the world
     var world = HittableList.init(allocator);
     try world.add(.{ .sphere = Sphere.init(Point.at(0.0, 0.0, -1.0), 0.5) });
     try world.add(.{ .sphere = Sphere.init(Point.at(0.0, -100.5, -1.0), 100.0) });
 
+    // render the world
+    const camera = Camera.init(16.0 / 9.0, 1920);
+    const color_array = try camera.render(&world, allocator);
+
     var buffer_writer = std.io.bufferedWriter(std.io.getStdOut().writer());
     var writer = buffer_writer.writer();
 
-    try writePPM(&writer, &world);
+    try writePPM(&writer, color_array);
 
     try buffer_writer.flush();
 
