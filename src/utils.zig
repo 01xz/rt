03d4d8f84ea499b39cbed2337fd31ecd987f327c
01xz <73350430+01xz@users.noreply.github.com>
2023-12-13 16:04:36 +0800

    fix: random generator should init outside

--- src/utils.zig
+++ src/utils.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 
-pub fn randomDouble() f64 {
-    const RandomGen = std.rand.DefaultPrng;
-    var rng = RandomGen.init(@bitCast(std.time.timestamp()));
-    return rng.random().float(f64);
+pub const RandomGen = std.rand.DefaultPrng;
+
+pub fn getRandom(rng: *RandomGen, comptime T: type) T {
+    return switch (@typeInfo(T)) {
+        .ComptimeFloat, .Float => rng.random().float(T),
+        .ComptimeInt, .Int => return rng.random().int(T),
+        else => @compileError("getRandom: " ++ @typeName(T) ++ " is not supported"),
+    };
 }
